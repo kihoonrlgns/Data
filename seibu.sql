dcd5f1e4fc2242b826944514034d22ae237e746a
Kihoon Lee <36075511+kihoontt@users.noreply.github.com>
2021-02-28 17:25:13 +0900

    Add files via upload

---
+++ seibu.sql
@@ -0,0 +1,43 @@
+--- Seibu session ---
+CREATE TABLE k_seibu_session_201912 AS
+SELECT wifi_id, shop_id, area, dwell_time, date, time, hour, country, row_number() over () as r_no
+    FROM (SELECT wifi_id, shop_id, area, dwell_time, ts,
+        substr(from_unixtime(CAST(ts / 1000 + 32400 as BIGINT), 'yyyy-MM-dd HH:mm:ss'),1,10) AS date,
+        substr(from_unixtime(CAST(ts / 1000 + 32400 as BIGINT), 'yyyy-MM-dd HH:mm:ss'),12,8) AS time,
+        substr(from_unixtime(CAST(ts / 1000 + 32400 as BIGINT), 'yyyy-MM-dd HH:mm:ss'),12,2) AS hour, country
+        FROM wifi_sessions
+        WHERE ((key >= '100:') and (key < '103:x'))
+        AND shop_id in (100,101,102,103) --1218,1435,2218,2236,2253
+        AND area in ('out')
+        AND deny is NULL
+        AND local = false) A
+        WHERE date >= '2019-12-01'
+        AND date <= '2019-12-31';
+        
+		
+SELECT wifi_id, shop_id, dwell_time, date, time, country
+FROM k_seibu_session_201912
+GROUP BY wifi_id, shop_id, dwell_time, date, time, country;
+
+
+CREATE TABLE k_seibu_session_201912 AS
+SELECT *
+FROM k_seibu_session_201912_1
+UNION ALL
+SELECT *
+FROM k_seibu_session_201912_2
+UNION ALL
+SELECT *
+FROM k_seibu_session_201912_3
+UNION ALL
+SELECT *
+FROM k_seibu_session_201912_4
+UNION ALL
+SELECT *
+FROM k_seibu_session_201912_5
+UNION ALL
+SELECT *
+FROM k_seibu_session_201912_6
+UNION ALL
+SELECT *
+FROM k_seibu_session_201912_7
